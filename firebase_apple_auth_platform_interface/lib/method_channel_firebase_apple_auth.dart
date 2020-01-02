9940bba34649eec4c5a095e4f2c88c4055621034
Amr Yousef <amr.farid140@gmail.com>
2020-01-02 18:23:48 +0000

    Created method channel implementation

---
+++ firebase_apple_auth_platform_interface/lib/method_channel_firebase_apple_auth.dart
@@ -0,0 +1,14 @@
+part of firebase_apple_auth_platform_interface;
+
+class MethodChannelFirebaseAppleAuth extends FirebaseAppleAuth {
+  static const MethodChannel _channel =
+  const MethodChannel('me.amryousef.apple.auth/firebase_apple_auth');
+
+  MethodChannelFirebaseAppleAuth._({FirebaseApp app}) : super._(app: app);
+
+  @override
+  Future<FirebaseUser> openSignInFlow() async {
+    await _channel.invokeMethod("openSignInFlow", {'app': _app.name});
+    return FirebaseAuth.fromApp(_app).currentUser();
+  }
+}
