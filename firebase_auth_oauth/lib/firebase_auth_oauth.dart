a90be679a4db4ab38f6f0392002ea63eea07f141
Amr Yousef <amr.farid140@gmail.com>
2020-09-05 22:23:40 +0100

    Use  User instead of FirebaseUser

--- firebase_auth_oauth/lib/firebase_auth_oauth.dart
+++ firebase_auth_oauth/lib/firebase_auth_oauth.dart
@@ -1,8 +1,8 @@
 library firebase_apple_auth;
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_oauth_platform_interface/firebase_auth_oauth_platform_interface.dart'
     as platform;
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 class FirebaseAuthOAuth implements platform.FirebaseAuthOAuth {
@@ -10,13 +10,19 @@ class FirebaseAuthOAuth implements platform.FirebaseAuthOAuth {
 
   FirebaseAuthOAuth({FirebaseApp app})
       : _delegate = app != null
-            ? platform.FirebaseAuthOAuth.instance.withApp(app)
-            : platform.FirebaseAuthOAuth.instance;
+      ? platform.FirebaseAuthOAuth.instance.withApp(app)
+      : platform.FirebaseAuthOAuth.instance;
 
   @override
-  Future<FirebaseUser> openSignInFlow(String provider, List<String> scopes,
-          [Map<String, String> customOAuthParameters]) =>
+  Future<User> openSignInFlow(String provider, List<String> scopes,
+      [Map<String, String> customOAuthParameters]) =>
       _delegate.openSignInFlow(provider, scopes, customOAuthParameters);
+
+  @override
+  Future<User> linkExistingUserWithCredentials(String provider,
+      List<String> scopes, [Map<String, String> customOAuthParameters]) =>
+      _delegate.linkExistingUserWithCredentials(
+          provider, scopes, customOAuthParameters);
 
   @override
   platform.FirebaseAuthOAuth withApp(FirebaseApp app) =>
