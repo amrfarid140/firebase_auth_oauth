4fc2deb83f0ef675d933d8b7c20f4f3ed757f867
Camillo Bucciarelli <camillo.bucciarelli@aesys.tech>
2020-11-23 21:47:22 +0100

    try fix ios crash

--- firebase_auth_oauth/ios/Classes/FirebaseAuthOAuthViewController.swift
+++ firebase_auth_oauth/ios/Classes/FirebaseAuthOAuthViewController.swift
@@ -48,19 +48,17 @@ public class FirebaseAuthOAuthViewController: UIViewController, FlutterPlugin {
 	func consumeCredentials(_ credential: AuthCredential) {
 		if call?.method == FirebaseAuthOAuthViewController.CREATE_USER_METHOD {
 			Auth.auth().signIn(with: credential) { authResult, error in
+                guard let currentUser = Auth.auth().currentUser else {
+                    self.finalizeResult(.PluginError(error: "currentUser is nil. Make sure a user exists when \(FirebaseAuthOAuthViewController.CREATE_USER_METHOD) is used."))
+                    return
+                }
 				if let firebaseError = error {
 					self.finalizeResult(
 						FirebaseAuthOAuthPluginError
 							.FirebaseAuthError(error: firebaseError)
 					)
 				}
-                guard let currentUser = Auth.auth().currentUser else {
-                    self.finalizeResult(
-                        FirebaseAuthOAuthPluginError
-                            .PluginError(error: "User null")
-                    )
-                }
-				self.finalizeResult(Auth.auth().currentUser)
+				self.finalizeResult(currentUser)
 			}
 		}
 		if call?.method == FirebaseAuthOAuthViewController.LINK_USER_METHOD {
