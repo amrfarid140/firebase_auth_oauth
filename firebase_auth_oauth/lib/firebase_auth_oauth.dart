9defe1bcd26266ec30243126c1c7e1ed4877b24d
Camillo Bucciarelli <camillo.bucciarelli@aesys.tech>
2020-11-25 11:55:38 +0100

    required fix

--- firebase_auth_oauth/lib/firebase_auth_oauth.dart
+++ firebase_auth_oauth/lib/firebase_auth_oauth.dart
@@ -1,25 +1,31 @@
 library firebase_apple_auth;
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_auth_oauth_platform_interface/firebase_auth_oauth_platform_interface.dart' as platform;
+import 'package:firebase_auth_oauth_platform_interface/firebase_auth_oauth_platform_interface.dart'
+    as platform;
 import 'package:firebase_core/firebase_core.dart';
 
 class FirebaseAuthOAuth implements platform.FirebaseAuthOAuth {
   final platform.FirebaseAuthOAuth _delegate;
 
   FirebaseAuthOAuth({FirebaseApp app})
-      : _delegate =
-            app != null ? platform.FirebaseAuthOAuth.instance.withApp(app) : platform.FirebaseAuthOAuth.instance;
+      : _delegate = app != null
+            ? platform.FirebaseAuthOAuth.instance.withApp(app)
+            : platform.FirebaseAuthOAuth.instance;
 
   @override
-  Future<User> openSignInFlow(String provider, List<String> scopes, [Map<String, String> customOAuthParameters]) =>
+  Future<User> openSignInFlow(String provider, List<String> scopes,
+      [Map<String, String> customOAuthParameters]) =>
       _delegate.openSignInFlow(provider, scopes, customOAuthParameters);
 
   @override
-  Future<User> linkExistingUserWithCredentials(String provider, List<String> scopes,
-          [Map<String, String> customOAuthParameters]) =>
-      _delegate.linkExistingUserWithCredentials(provider, scopes, customOAuthParameters);
+  Future<User> linkExistingUserWithCredentials(String provider,
+      List<String> scopes,
+      [Map<String, String> customOAuthParameters]) =>
+      _delegate.linkExistingUserWithCredentials(
+          provider, scopes, customOAuthParameters);
 
   @override
-  platform.FirebaseAuthOAuth withApp(FirebaseApp app) => FirebaseAuthOAuth(app: app);
+  platform.FirebaseAuthOAuth withApp(FirebaseApp app) =>
+      FirebaseAuthOAuth(app: app);
 }
